bf95b206f13e519653518b36e5aa56cd5236d41c
sebma <sebma@users.noreply.github.com>
2020-11-18 13:28:00 +0100

    Updated Microsoft.PowerShell_profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,29 @@
 # $HOME/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
-$PowerShellUserConfigDIR="$HOME/.config/powershell"
-Import-Alias $PowerShellUserConfigDIR/seb_aliases.ps1
-Set-Alias vi "$env:ProgramFiles/Git/usr/bin/vim.exe"
-Set-Alias  ex
+#
+function osFamily {
+	$osFamily = "Linux"
+	return $osFamily
+}
+
+$osFamily = osFamily
+if( $osFamily -eq "Linux") {
+	$PowerShellUserConfigDIR="$HOME/.config/powershell"
+} elseif( $osFamily -eq "Windows") {
+	Set-Alias vi "$env:ProgramFiles/Git/usr/bin/vim.exe"
+	Set-Alias  ex
+} elseif( $osFamily -eq "Darwin") {
+	echo TO DO
+}
+
+Import-Alias "$PowerShellUserConfigDIR/seb_${osFamily}_aliases.ps1"
 
 function Prompt {
     $mywd = (pwd).Path
     $mywd = $mywd.Replace( $HOME, '~' )
 #    $PSHVersion = $PSVersionTable.PSVersion.ToString()
     $PSHVersion = [String]$PSVersionTable.PSVersion.Major + "." + $PSVersionTable.PSVersion.Minor
-    Write-Host "PSv$PSHVersion " -NoNewline -ForegroundColor DarkGreen
+    Write-Host "[$osFamily] " -NoNewline -ForegroundColor DarkRed
+    Write-Host "PSv$PSHVersion " -NoNewline -ForegroundColor DarkBlue
     Write-Host ("" + $mywd + ">") -NoNewline -ForegroundColor Green
     return " "
 }
@@ -18,8 +32,9 @@ function gitUpdate {
 	echo "=> Updating from : "
 	git config remote.origin.url
 	git pull
-#	sync
+	if( $osFamily -eq "Linux"){sync}
 }
+
 
 function ex{exit}
 function cds($p){if($p -eq "-"){popd} else {pushd $p}}
