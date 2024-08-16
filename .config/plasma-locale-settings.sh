7cdae5082c85edf72a0391ee5f6bf9f647bf2eca
sebma <sebma@users.noreply.github.com>
2024-08-17 00:31:37 +0200

    Updated plasma-locale-settings.sh

---
+++ .config/plasma-locale-settings.sh
@@ -0,0 +1,11 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_GB.UTF-8
+export LC_NUMERIC=C
+export LC_TIME=fr_FR.UTF-8
+export LC_MONETARY=fr_FR.UTF-8
+export LC_MEASUREMENT=fr_FR.UTF-8
+export LC_COLLATE=C
+export LANGUAGE=en_US
