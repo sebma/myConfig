0c8fbf8175d16800f734d5a1f241fc11df442898
sebma <sebma@users.noreply.github.com>
2025-04-04 20:14:45 +0200

    Update install-podman_opensuse.sh

--- linux/ubuntu/other_repos/install-podman_opensuse.sh
+++ linux/ubuntu/other_repos/install-podman_opensuse.sh
@@ -4,6 +4,6 @@ installPackagesFromRepo.sh "http://download.opensuse.org/repositories/devel:/kub
 if ! grep $USER /etc/subuid;then
 	start=$(awk -F: '/^[a-zA-Z]/{start=$2+$3}END{printf start}' /etc/subuid)
  	count=$((2**16))
-  	last=$(($start+$count-1))
+	last=$(($start+$count-1))
 	usermod --add-subuids $start-$last --add-subgids $start-$last $USER
 fi
