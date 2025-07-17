a9e886cb85375f2800e9e65444c67600e84e0446
sebma <sebma@users.noreply.github.com>
2025-07-17 23:23:24 +0200

    Updated seb_Windows_aliases.ps1

--- .config/powershell/seb_Windows_aliases.ps1
+++ .config/powershell/seb_Windows_aliases.ps1
@@ -1,6 +1,6 @@
 "env","Get-Variable","","ReadOnly, AllScope"
+"printenv","gci env:","","ReadOnly, AllScope"
 "np","notepad","","ReadOnly, AllScope"
 "unzip","expand-archive","","ReadOnly, AllScope"
-#"grep","findstr","","ReadOnly, AllScope"
 "grep","select-string","","ReadOnly, AllScope"
 "man","get-help","","ReadOnly, AllScope"
