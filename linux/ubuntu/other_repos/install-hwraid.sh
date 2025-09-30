ce789a6a7e2f7782efdbc490720cc2e7f283960a
sebma <sebma@users.noreply.github.com>
2025-09-30 11:18:05 +0200

    Create install-hwraid.sh

---
+++ linux/ubuntu/other_repos/install-hwraid.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+dmesg | grep SmartArray -q || installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" hpacucli
+dmesg | grep MegaRAID.SAS -q || installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" megacli
