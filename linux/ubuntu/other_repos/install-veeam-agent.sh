243132a923357bc6dd2e73f0b642c0c38838030c
sebma <sebma@users.noreply.github.com>
2025-10-08 14:41:39 +0200

    Update install-veeam-agent.sh

--- linux/ubuntu/other_repos/install-veeam-agent.sh
+++ linux/ubuntu/other_repos/install-veeam-agent.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-installPackagesFromRepo.sh "https://repository.veeam.com/backup/linux/agent/dpkg/debian/public stable veeam" blksnap veeam
+installPackagesFromRepo.sh "https://repository.veeam.com/backup/linux/agent/dpkg/debian/public stable veeam" veeam-release-deb blksnap veeam
