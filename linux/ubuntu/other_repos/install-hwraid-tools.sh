d2948d1cbec0243e04446b425854560d14529e6e
sebma <sebma@users.noreply.github.com>
2025-10-08 11:49:35 +0200

    Update install-hwraid-tools.sh

--- linux/ubuntu/other_repos/install-hwraid-tools.sh
+++ linux/ubuntu/other_repos/install-hwraid-tools.sh
@@ -4,6 +4,6 @@
 raidDriversList=$(\lspci -d::0104 -k | awk '/Kernel driver in use/{print\$NF}' | grep -vw ahci | head -1)
 case $raidDriversList in
 	hpsa) installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" hpacucli;;
-	mega????) installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" megacli;;
+	megaraid_sas) installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" megacli megactl  megaclisas-status;;
 	*) echo "=> $raidDriversList is not supported yes" >&2;exit 1;;
 esac
