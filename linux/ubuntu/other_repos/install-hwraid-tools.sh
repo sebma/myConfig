31345f7952e2964106e65ca9c5ec3257adf4ca4e
sebma <sebma@users.noreply.github.com>
2025-10-08 11:34:47 +0200

    Update install-hwraid-tools.sh

--- linux/ubuntu/other_repos/install-hwraid-tools.sh
+++ linux/ubuntu/other_repos/install-hwraid-tools.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# cf. https://github.com/eLvErDe/hwraid and https://hwraid.le-vert.net/wiki/DebianPackages
 dmesg | grep SmartArray -q || installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" hpacucli
 dmesg | grep MegaRAID.SAS -q || installPackagesFromRepo.sh "deb http://hwraid.le-vert.net/ubuntu $(lsb_release -sc) main" megacli
