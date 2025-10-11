8ce74b895f0cfd1b68a37daa7cb2ac6cc5b58db0
sebma <sebma@users.noreply.github.com>
2025-10-11 12:29:57 +0200

    Updated install-podman_opensuse.sh

--- linux/ubuntu/other_repos/install-podman_opensuse.sh
+++ linux/ubuntu/other_repos/install-podman_opensuse.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-installPackagesFromRepo.sh "http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$(lsb_release -sr)/ /" podman
+if ! apt-cache -q=0 policy podman-compose 2>&1 | grep '^N:' -q;then
+	sudo apt install -V podman podman-compose
+else
+installPackagesFromRepo.sh "http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$(lsb_release -sr)/ /" podman podman-compose
 if ! grep $USER /etc/subuid;then
 	start=$(awk -F: '/^[a-zA-Z]/{start=$2+$3}END{printf start}' /etc/subuid)
  	count=$((2**16))
 	last=$(($start+$count-1))
 	usermod --add-subuids $start-$last --add-subgids $start-$last $USER
+fi
 fi
