de1b3fc3da98813d92a1fd15fbb5fe3e4408dd4b
sebma <sebma@users.noreply.github.com>
2026-01-04 18:37:07 +0100

    Updated install-rustdesk.sh

--- linux/ubuntu/other_repos/install-rustdesk.sh
+++ linux/ubuntu/other_repos/install-rustdesk.sh
@@ -2,4 +2,4 @@
 
 sudo wget -O /usr/share/keyrings/newlifelinux.gpg http://ppa.newlifelinux.org/noble/newlifelinux.gpg
 sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/newlifelinux.gpg] http://ppa.newlifelinux.org/ubuntu/ noble main" | sudo tee /etc/apt/sources.list.d/newlife-noble.list
-sudo apt update && sudo apt install -V betterbird -y
+sudo apt update && sudo apt install -V rustdesk -y
