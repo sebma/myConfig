41e2340d72513a7720df307f8d70308f1329e5e0
sebma <sebma@users.noreply.github.com>
2026-02-13 14:31:50 +0100

    Update install-python_deadsnakes.sh

--- linux/ubuntu/launchpad_PPAs/install-python_deadsnakes.sh
+++ linux/ubuntu/launchpad_PPAs/install-python_deadsnakes.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-installPackagesFromRepo.sh ppa:deadsnakes/ppa python3.14
+set -o nounset
+
+scriptBaseName=${0/*\//}
+
+if [ $# -eq 0 ];then
+        echo "=> Usage: $scriptBaseName pythonVersion" >&2
+        exit 1
+fi
+
+pythonVersion=$1
+if grep [0-9].[0-9] -q <<< $pythonVersion;then
+        echo installPackagesFromRepo.sh ppa:deadsnakes/ppa python-$pythonVersion
+else
+        echo "=> Please enter version in the form digit.digit" >&2
+        exit 2
+fi
