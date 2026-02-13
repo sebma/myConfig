32778e00e94f1d68ad6714f8ceda06e563a5389c
sebma <sebma@users.noreply.github.com>
2026-02-13 14:33:08 +0100

    Update install-haproxy_vbernat.sh

--- linux/ubuntu/launchpad_PPAs/install-haproxy_vbernat.sh
+++ linux/ubuntu/launchpad_PPAs/install-haproxy_vbernat.sh
@@ -5,13 +5,13 @@ set -o nounset
 scriptBaseName=${0/*\//}
 
 if [ $# -eq 0 ];then
-        echo "=> Usage: $scriptBaseName haproxyVersion" >&2
+        echo "=> Usage: $scriptBaseName version" >&2
         exit 1
 fi
 
-haproxyVersion=$1
-if grep [0-9].[0-9] -q <<< $haproxyVersion;then
-        installPackagesFromRepo.sh ppa:vbernat/haproxy-$haproxyVersion haproxy
+version=$1
+if grep [0-9].[0-9] -q <<< $version;then
+        installPackagesFromRepo.sh ppa:vbernat/haproxy-$version haproxy
 else
         echo "=> Please enter version in the form digit.digit" >&2
         exit 2
