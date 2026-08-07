f08ec07978d137ec9b8f4b544c1611d6c4f3463d
sebma <sebma@users.noreply.github.com>
2026-08-07 16:43:05 +0200

    Update install-powershell.sh

--- linux/ubuntu/other_repos/install-powershell.sh
+++ linux/ubuntu/other_repos/install-powershell.sh
@@ -3,11 +3,14 @@
 ubuntuRelease=$(lsb_release -sr)
 ubuntuCodeName=$(lsb_release -sc)
 if ! dpkg -s packages-microsoft-prod &>/dev/null;then
-        wget https://packages.microsoft.com/config/ubuntu/$ubuntuRelease/packages-microsoft-prod.deb
-        sudo apt install -V ./packages-microsoft-prod.deb
-        rm -v ./packages-microsoft-prod.deb
+	wget https://packages.microsoft.com/config/ubuntu/$ubuntuRelease/packages-microsoft-prod.deb
+	sudo apt install -V ./packages-microsoft-prod.deb
+	rm -v ./packages-microsoft-prod.deb
+	sudo sed -i "/$ubuntuRelease/s/$ubuntuRelease/24.04/;/$ubuntuCodeName/s/$ubuntuCodeName/noble/" /etc/apt/sources.list.d/microsoft-prod.list
+	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg --yes
 fi
 
-sudo sed -i "/$ubuntuRelease/s/$ubuntuRelease/24.04/;/$ubuntuCodeName/s/$ubuntuCodeName/noble/" /etc/apt/sources.list.d/microsoft-prod.list
-sudo apt update
-sudo apt install -V powershell
+if ! dpkg -s powershell &>/dev/null;then
+	sudo apt update
+	sudo apt install -V powershell
+fi
