fce245d5ed1750651ffd31d89cb855bcbb5934e0
sebma <sebma@users.noreply.github.com>
2026-08-17 10:46:08 +0200

    Update install-msteams.sh

--- linux/ubuntu/other_repos/install-msteams.sh
+++ linux/ubuntu/other_repos/install-msteams.sh
@@ -9,9 +9,9 @@ if ! dpkg -s packages-microsoft-prod &>/dev/null;then
 	sudo sed -i "/$ubuntuRelease/s/$ubuntuRelease/24.04/;/$ubuntuCodeName/s/$ubuntuCodeName/noble/" /etc/apt/sources.list.d/microsoft-prod.list
 	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg --yes
 fi
-
+arch=$(dpkg --print-architecture)
 if ! dpkg -s ms-teams &>/dev/null;then
-	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-prod.gpg] https://packages.microsoft.com/repos/ms-teams stable main" \
+	echo "deb [arch=$arch signed-by=/usr/share/keyrings/microsoft-prod.gpg] https://packages.microsoft.com/repos/ms-teams stable main" \
 | sudo tee /etc/apt/sources.list.d/ms-teams.list
 	sudo apt update
 	sudo apt install -V ms-teams
