c205ebba1e769490e3bd79def86702797d0db765
sebma <sebma@users.noreply.github.com>
2026-08-17 10:39:14 +0200

    Update install-powershell.sh

--- linux/ubuntu/other_repos/install-powershell.sh
+++ linux/ubuntu/other_repos/install-powershell.sh
@@ -8,7 +8,7 @@ if ! dpkg -s packages-microsoft-prod &>/dev/null;then
 	rm -v ./packages-microsoft-prod.deb
 	sudo sed -i "/$ubuntuRelease/s/$ubuntuRelease/24.04/;/$ubuntuCodeName/s/$ubuntuCodeName/noble/" /etc/apt/sources.list.d/microsoft-prod.list
 #	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg --yes
-	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft-prod.gpg --yes
+#	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft-prod.gpg --yes
 fi
 
 if ! dpkg -s powershell &>/dev/null;then
