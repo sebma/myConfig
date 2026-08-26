fe021091089d60af124b2a10a5f9498a178522d1
sebma <sebma@users.noreply.github.com>
2026-08-26 22:33:17 +0200

    Updated install-google-android-emulator-installer_bastif-google-android-sdk.sh

---
+++ linux/ubuntu/launchpad_PPAs/install-google-android-emulator-installer_bastif-google-android-sdk.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+installPackagesFromRepo.sh ppa:bastif/google-android-sdk google-android-build-tools-34.0.0-installer google-android-licenses google-android-platform-tools-installer
