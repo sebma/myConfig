1a04433d6d0e7516ee71b49efcdaf03a9b7deaaf
sebma <sebma@users.noreply.github.com>
2026-08-31 21:18:09 +0200

    Updated install-rustdesk.sh

--- linux/ubuntu/other_repos/install-rustdesk.sh
+++ linux/ubuntu/other_repos/install-rustdesk.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo wget -O /usr/share/keyrings/newlifelinux.gpg http://ppa.newlifelinux.org/noble/newlifelinux.gpg
+sudo wget -O /usr/share/keyrings/newlifelinux.gpg http://ppa.newlifelinux.org/gpg.key
 sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/newlifelinux.gpg] http://ppa.newlifelinux.org/ubuntu/ noble main" | sudo tee /etc/apt/sources.list.d/newlife-noble.list
 sudo apt update && sudo apt install -V rustdesk -y
