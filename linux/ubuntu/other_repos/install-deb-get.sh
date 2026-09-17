a3a7d6f2e79f9ab775cfff75e2cb1fc3d4a1af3a
sebma <sebma@users.noreply.github.com>
2026-09-18 00:06:08 +0200

    Updated install-deb-get.sh

--- linux/ubuntu/other_repos/install-deb-get.sh
+++ linux/ubuntu/other_repos/install-deb-get.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install curl lsb-release wget jq -Vy
-curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+if ! which deb-get >/dev/null 2>&1;then
+	sudo apt install curl lsb-release wget jq -Vy
+	curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+fi
