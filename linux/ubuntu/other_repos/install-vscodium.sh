398fef2d02b61f8efcd1899a913fdf2e05925d6f
sebma <sebma@users.noreply.github.com>
2026-09-17 21:45:28 +0200

    Updated install-vscodium.sh

---
+++ linux/ubuntu/other_repos/install-vscodium.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ubuntuRelease=$(lsb_release -sr)
+ubuntuCodeName=$(lsb_release -sc)
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+arch=$(dpkg --print-architecture)
+if ! dpkg -s codium &>/dev/null;then
+	echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+| sudo tee /etc/apt/sources.list.d/codium.list
+	apt policy codium | grep 'Candidate:' -q || sudo apt update
+	sudo apt install -V codium codium-exploration codium-insiders
+fi
+
+#xdg-mime default code.desktop text/plain
+#sudo update-alternatives --install $(type -P editor) editor $(which codium) 10
+#sudo update-alternatives --set editor $(type -P codium)
