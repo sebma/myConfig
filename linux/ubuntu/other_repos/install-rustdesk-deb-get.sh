348b088666c37d13588b043d2268f0b2876bce1d
sebma <sebma@users.noreply.github.com>
2026-09-18 00:06:08 +0200

    Updated install-rustdesk-deb-get.sh

---
+++ linux/ubuntu/other_repos/install-rustdesk-deb-get.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if ! which deb-get >/dev/null 2>&1;then
+	sudo apt install curl lsb-release wget jq -Vy
+	curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+fi
+sudo deb-get install rustdesk
